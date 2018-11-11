bc219e905833e00f16cf25869d05b1bfb3e8b56b
Chiorufarewerin <artur1998g@gmail.com>
2018-11-11 16:48:02 +0300

    Some changes

--- setup.py
+++ setup.py
@@ -1,20 +1,30 @@
 '''Setup file'''
 
-import setuptools
+import os
+import sys
+from distutils.sysconfig import get_python_lib
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import find_packages, setup
 
-setuptools.setup(
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+setup(
     name="CRUD-Wishlist",
     version="0.2",
     author="Chiorufarewerin",
+    python_requires='>=3.5',
     author_email="artur1998g@gmail.com",
     description="Simple CRUD application on Python with PyQT5.",
-    long_description=long_description,
+    long_description=read('README.md'),
     long_description_content_type="text/markdown",
+    license="MIT",
+    platform="Windows",
     url="https://github.com/Chiorufarewerin/CRUD-Wishlist",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         "Programming Language :: Python :: 3",
