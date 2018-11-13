ddfd78f0f7e9bc4530d89c3f306fbd088488e6e6
Chiorufarewerin <artur1998g@gmail.com>
2018-11-13 07:41:08 +0300

    Added resize photo in centr

--- CRUD-Wishlist/app/MainWindow.py
+++ CRUD-Wishlist/app/MainWindow.py
@@ -10,6 +10,34 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
 
+    def resizeEvent(self, a0):
+        photo_size = self.PhotoLabel.size()
+        max = photo_size.height()
+        if max > photo_size.width():
+            max = photo_size.width()
+        if (self.PhotoLabel.pixmap() is not None
+                and not self.PhotoLabel.pixmap().isNull()):
+            pixmap = self.PhotoLabel.pixmap()
+            image_size = pixmap.size()
+            if image_size.height() > max:
+                max = image_size.height()
+            if image_size.width() > max:
+                max = image_size.width()
+        layout = self.PhotoFrame.layout()
+        left, top, right, bottom = layout.getContentsMargins()
+        left = right = (self.PhotoFrame.size().width() - 40 - max) // 2
+        top = bottom = (self.PhotoFrame.size().height() - max) // 2
+        print(left, top, right, bottom, max)
+        if left < 20:
+            left, right, top, bottom = 20, 20, top, bottom
+        if top < 20:
+            left, right, top, bottom = left, right, 20, 20
+        layout.setContentsMargins(left, top, right, bottom)
+
+    def showEvent(self, a0):
+        super().showEvent(a0)
+        self.resizeEvent(None)
+
 
 def start(argv):
     '''Start application function.'''
